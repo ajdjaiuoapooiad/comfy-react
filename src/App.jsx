d1983d49679b4512b67afa66fc34f29d7129727f
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-15 14:14:14 +0900

    routes ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,52 @@
+import { createBrowserRouter } from 'react-router-dom'
+import {About,Cart,Checkout,Error,HomeLayout,Landing,Login,Order,Products,Register,SingleProduct} from './pages'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'orders',
+        element: <Order />,
+      },
+      
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+])
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
