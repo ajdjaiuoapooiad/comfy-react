9f5c00b23ba3bcd8c3a69ceea295eb1ead78e150
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-15 16:10:59 +0900

    login ok

---
+++ src/components/SubmitBtn.jsx
@@ -0,0 +1,9 @@
+
+
+const SubmitBtn = () => {
+  return (
+    <div>SubmitBtn</div>
+  )
+}
+
+export default SubmitBtn
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import {default as FormInput} from './FormInput';
+import {default as SubmitBtn} from './SubmitBtn';
+
