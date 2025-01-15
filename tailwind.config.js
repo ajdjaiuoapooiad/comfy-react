d31de267f7d73f48ff50c06e3a94eb5041ee491e
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-15 16:21:04 +0900

    error ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './index.html', './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'), require('daisyui')
+    require('@tailwindcss/typography'), require('daisyui'),
   ],
 }
 
