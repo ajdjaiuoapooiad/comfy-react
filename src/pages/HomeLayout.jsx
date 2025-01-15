aa3411f43f7d2b46fe6335f3f217d38ab5047c72
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-15 17:35:12 +0900

    nab ok

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
     return (
       <>
         <Header />
+        <Navbar />
         <section className="align-element py-20">
 
         <Outlet />
