587e5204497cd2993e394e8abc2be920cd50f617
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-16 16:33:57 +0900

    selectInput ok

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,15 +1,16 @@
-
-
-const FormInput = ({label,name,type,defaultValue}) => {
+const FormInput = ({ label, name, type, defaultValue, size }) => {
   return (
-    <label className="form-control  ">
-        <div className="label">
-            <span className="label-text">{label}</span>
-        </div>
-        <input type={type} name={name} defaultValue={defaultValue} className="input input-bordered  " />
-        
-    </label>
-  )
-}
-
-export default FormInput
+    <div className='form-control'>
+      <label htmlFor={name} className='label'>
+        <span className='label-text capitalize'>{label}</span>
+      </label>
+      <input
+        type={type}
+        name={name}
+        defaultValue={defaultValue}
+        className={`input input-bordered ${size}`}
+      />
+    </div>
+  );
+};
+export default FormInput;
